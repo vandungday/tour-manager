33224624a84fa72dea75e00605c2d7e6ca8f0a79
vandungday <dungtn492@gmail.com>
2022-12-20 16:01:12 +0700

    ref #4 delete a tour

--- controllers/tour.controller.js
+++ controllers/tour.controller.js
@@ -1,5 +1,6 @@
 const Tour = require('../models/Tours');
 const asyncHandler = require('../middlewares/asyncHandler');
+const ErrorResponse = require('../middlewares/ErrorResponse');
 
 exports.createTour = asyncHandler(async (req, res, next) => {
   const newTour = await Tour.create(req.body);
@@ -38,10 +39,28 @@ exports.getTour = asyncHandler(async (req, res, next) => {
 exports.updateTour = asyncHandler(async (req, res, next) => {
   const tour = await Tour.findByIdAndUpdate(req.params.id, req.body);
 
+  if (!tour) {
+    return next(new ErrorResponse('No tour found with that ID', 404));
+  }
+
   res.status(200).json({
     status: 'success',
     data: {
       tour,
     },
+  });
+});
+
+exports.deleteTour = asyncHandler(async (req, res, next) => {
+  const tour = await Tour.findByIdAndDelete(req.params.id);
+
+  console.log(tour);
+  if (!tour) {
+    return next(new ErrorResponse('No tour found with that ID', 404));
+  }
+
+  res.status(204).json({
+    status: 'success',
+    data: null,
   });
 });
--- routes/v1/tour.route.js
+++ routes/v1/tour.route.js
@@ -11,6 +11,7 @@ router
 router
   .route('/:id')
   .get(tourController.getTour)
-  .patch(tourController.updateTour);
+  .patch(tourController.updateTour)
+  .delete(tourController.deleteTour);
 
 module.exports = router;
