1e8a1b7d0ec8ac3463c0854802f5870646fec535
vandungday <dungtn492@gmail.com>
2022-12-20 16:02:32 +0700

    ref #5 get a tour

--- controllers/tour.controller.js
+++ controllers/tour.controller.js
@@ -28,6 +28,10 @@ exports.getAllTours = asyncHandler(async (req, res, next) => {
 exports.getTour = asyncHandler(async (req, res, next) => {
   const tour = await Tour.findById(req.params.id);
 
+  if (!tour) {
+    return next(new ErrorResponse('No tour found with that ID', 404));
+  }
+
   res.status(200).json({
     status: 'success',
     data: {
